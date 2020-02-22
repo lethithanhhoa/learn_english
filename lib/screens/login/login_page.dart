7d7d5f18a353b4746ac0dedcb054deb963d4cc19
lethithanhhoa <lethithanhhoa0106@gmail.com>
2020-02-22 23:58:16 +0700

    add auth sevice for login feature

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,13 +11,67 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _message = 'You are not sign in';
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FacebookLogin _facebooklogin = FacebookLogin();
+
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
+    setState(() {
+      _message = "You are signed in";
+    });
+    return user;
+  }
+
+  Future _checkLogin() async {
+    final FirebaseUser user = await _auth.currentUser();
+    if (user != null) {
+      setState(() {
+        _message = "You are signed in";
+      });
+    }
+  }
+
+  Future _loginWithFacebook() async {
+    final result = await _facebooklogin.logIn(['email']);
+
+    if (result.status == FacebookLoginStatus.loggedIn) {
+      final credential = FacebookAuthProvider.getCredential(
+        accessToken: result.accessToken.token,
+      );
+      final user = (await _auth.signInWithCredential(credential)).user;
+      setState(() {
+        _message = "Logged in as ${user.displayName}";
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _checkLogin();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(15, 35, 15, 35),
+          padding: EdgeInsets.fromLTRB(15, 50, 15, 50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
@@ -72,7 +129,9 @@ class _LoginPageState extends State<LoginPage> {
                           border: Border.all(width: 1.2, color: Colors.pink),
                           borderRadius: BorderRadius.circular(50)),
                       child: FlatButton(
-                        onPressed: null,
+                        onPressed: () {
+                          _loginWithFacebook();
+                        },
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
@@ -103,7 +162,9 @@ class _LoginPageState extends State<LoginPage> {
                           border: Border.all(width: 1.2, color: Colors.pink),
                           borderRadius: BorderRadius.circular(50)),
                       child: FlatButton(
-                        onPressed: null,
+                        onPressed: () {
+                          _handleSignIn();
+                        },
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
