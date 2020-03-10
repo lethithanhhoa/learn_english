8e59fda7d1d0fa6b8437d08e85a7cbbbc199b9f2
lethithanhhoa <lethithanhhoa0106@gmail.com>
2020-03-11 00:25:42 +0700

    change UI list of book

--- lib/widgets/book_list.dart
+++ lib/widgets/book_list.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:learn_english/models/book.dart';
 import 'package:learn_english/services/book_service.dart';
 import 'package:learn_english/widgets/unit_list.dart';
-class BookList extends StatefulWidget { 
-  BookList({ Key key}) : super(key : key);
+import 'package:learn_english/services/image_service.dart';
+import 'package:flutter_sequence_animation/flutter_sequence_animation.dart';
+
+class BookList extends StatefulWidget {
+  BookList({Key key}) : super(key: key);
   @override
   BookListState createState() => BookListState();
 }
@@ -15,113 +20,208 @@ class BookListState extends State<BookList> {
   bool loading = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     fetchBooks();
   }
 
-  Future<Null> fetchBooks() async{
-    this.setState((){
+  Future<Null> fetchBooks() async {
+    this.setState(() {
       loading = true;
     });
 
     List<Book> fetchedBooks = await _bookService.getListBook();
-    this.setState((){
+    this.setState(() {
       books = fetchedBooks;
       loading = false;
     });
   }
 
-	@override 
-	Widget build(BuildContext context) {
-		return Scaffold(
-			bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("Home"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.autorenew),
-          title: Text("Rankking"),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle),
-          title: Text("Account"),
-        ),
-      ]),
-			body: Stack(
-        children : <Widget>[
-          Container(
-              child: new Image.asset(
-            "assets/bg.jpg",
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.fill,
-          )),
-          Center(
-            child: new ListView.builder(
-              padding: EdgeInsets.all(10.0),
-              itemBuilder: _buildRow
-            ),
-			    ),
-          loading ? Stack(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar:
+            BottomNavigationBar(items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.autorenew),
+            title: Text("Rankking"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text("Account"),
+          ),
+        ]),
+        // appBar: AppBar(
+        //   title: Text("Home"),
+        //   // backgroundColor: Colors.white,
+        //   actions: <Widget>[
+        //     Icon(
+        //       FontAwesomeIcons.award,
+        //       color: Colors.white,
+        //     ),
+        //   ],
+        // ),
+        body: Stack(children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Opacity(
-                opacity: 0.7,
-                child: ModalBarrier(dismissible: false, color : Colors.black),
+              Container(
+                height: MediaQuery.of(context).size.height * 2/ 5,
+                decoration: BoxDecoration(
+                  // border: Border.all(color: Colors.red),
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(60.0)),
+                ),
+                child: Stack(children: <Widget>[
+                  
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(60.0)),
+                      image: DecorationImage(
+                        image: Image.asset("assets/ba18.jpg").image,
+                        fit: BoxFit.cover
+                      ),
+                    ),
+                  
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: MediaQuery.of(context).size.width - 80,
+                            height: 180,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                              BorderRadius.circular(10.0),
+                              image: DecorationImage(
+                                image: Image.asset("assets/ba17.jpg").image,
+                                fit: BoxFit.cover
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ]),
               ),
-              Center(
-                child: CircularProgressIndicator(),
-              )
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: Container(
+                  child: Text(
+                    'Courses',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: new ListView.builder(
+                  // scrollDirection: Axis.vertical,
+                  // padding: EdgeInsets.all(10.0),
+                  itemBuilder: _buildRow,
+                ),
+              ),
             ],
-          ) : Stack()
-        ]
-      )
-		);
-	}
+          ),
+          loading
+              ? Stack(
+                  children: <Widget>[
+                    Opacity(
+                      opacity: 0.7,
+                      child:
+                          ModalBarrier(dismissible: false, color: Colors.black),
+                    ),
+                    Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  ],
+                )
+              : Stack()
+        ]));
+  }
 
-	Widget _buildRow(context, index) {
-		if (index >= books.length) 
-			return null;
-
+  Widget _buildRow(context, index) {
+    if (index >= books.length) return null;
     Book currentBook = books[index];
-		return new ListTile(
-			title : Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-						margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
-						width: 100.0,
-						height: 100.0,
-						decoration: new BoxDecoration(
-							shape: BoxShape.circle,
-							image: DecorationImage(
-								fit: BoxFit.fill,
-								image: new NetworkImage('https://firebasestorage.googleapis.com/v0/b/learn-english-7c4c5.appspot.com/o/screen_2.jpg?alt=media&token=c478dbc4-78d9-4094-bef9-aeee13f31aad')
-							),
-							color: Colors.blue
-						),
-					),
-          Expanded(
-            child: Text(
-              '  ${currentBook.imgName}',
-              overflow: TextOverflow.fade,
-              maxLines: 1,
-              softWrap: false,
+    // String url = '';
+    // ImageService _imageService = new ImageService(url, currentBook.imgName);
+    // print("........................ ${currentBook.imgName}");
+    // print(_imageService.url);
+    return new ListTile(
+      title: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: Color.fromRGBO(255, 255, 255, 1.0),
+          gradient: LinearGradient(
+            // colors: [Colors.pink[50], Colors.pink[100], Colors.pink[50]],
+            // colors: [Colors.lime[100], Colors.lime, Colors.lime[100]],
+            colors: [Colors.lime, Colors.yellow, Colors.yellow[100]],
+            begin: Alignment.bottomLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 10.0),
+              width: 100.0,
+              height: 80.0,
+              decoration: new BoxDecoration(
+                  // border: Border.all(color: Colors.pink),
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(
+                          'https://firebasestorage.googleapis.com/v0/b/learn-english-7c4c5.appspot.com/o/screen_2.jpg?alt=media&token=c478dbc4-78d9-4094-bef9-aeee13f31aad')),
+                  color: Colors.blue),
+            ),
+            SizedBox(width: 15),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Container(
+                    child: Text(
+                  '${currentBook.bookTitle}',
+                  textAlign: TextAlign.start,
+                  style: new TextStyle(
+                    color: Colors.black54,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  // overflow: TextOverflow.fade,
+                )),
+              ),
             )
-          )
-        ],
+          ],
+        ),
       ),
-			onTap: () {
-				Navigator.push(context, 
-					MaterialPageRoute(builder: (context) => UnitList(
-            indexBookId: currentBook.docId,
-            bookTitle: currentBook.bookTitle,
-					)));
-			},
-		);
-	}
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => UnitList(
+                      indexBookId: currentBook.docId,
+                      bookTitle: currentBook.bookTitle,
+                    )));
+      },
+    );
+  }
 }
